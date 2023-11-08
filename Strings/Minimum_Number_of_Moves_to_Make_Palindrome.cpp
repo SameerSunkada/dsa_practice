cbdde9aa63c1aaebf5d465920fe30a0f1348adbc
SameerSunkada <sunkadasameer18@gmail.com>
2023-11-09 00:42:38 +0530

    updated Minimum Number of Moves to Make Palindrome

--- Strings/Minimum_Number_of_Moves_to_Make_Palindrome.cpp
+++ Strings/Minimum_Number_of_Moves_to_Make_Palindrome.cpp
@@ -1,23 +1,45 @@
-int minMovesToMakePalindrome(string s) {
-        int n = s.size();
-        int left = 0, right = n-1, count = 0;
-        while(left<right){
-            int l = left, r = right;
-            while(s[l] != s[r]) r--;
-            if(l==r){
-                swap(s[r], s[r+1]);
-                count++;
-                continue;
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        int ans = 0;
+        while (!s.empty()) {
+            int i = s.find(s.back());
+
+            if (i == s.length() - 1) {
+                ans += (i / 2);
+            } else {
+                ans += i;
+                s.erase(i, 1);
             }
-            else{
-                while(r<right){
-                    swap(s[r],s[r+1]);
-                    count++;
-                    r++;
-                }
-            }
-            left++;
-            right--;
+
+            s.pop_back();
         }
-        return count;
+
+        return ans;
     }
+};
+
+
+// int minMovesToMakePalindrome(string s) {
+//         int n = s.size();
+//         int left = 0, right = n-1, count = 0;
+//         while(left<right){
+//             int l = left, r = right;
+//             while(s[l] != s[r]) r--;
+//             if(l==r){
+//                 swap(s[r], s[r+1]);
+//                 count++;
+//                 continue;
+//             }
+//             else{
+//                 while(r<right){
+//                     swap(s[r],s[r+1]);
+//                     count++;
+//                     r++;
+//                 }
+//             }
+//             left++;
+//             right--;
+//         }
+//         return count;
+//     }
